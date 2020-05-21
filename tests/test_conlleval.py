7508183131c88dca14e05c3aeec9e61c84c1c26a
Sasha <q.dev.null@gmail.com>
2020-05-21 18:45:45 +0200

    Clean the tests: separate the sample reading from the test case

--- tests/test_conlleval.py
+++ tests/test_conlleval.py
@@ -4,7 +4,16 @@ import subprocess
 from conlleval import *
 
 
-def _test_case(lines):
+def gold(lines):
+    process = subprocess.Popen([
+        "perl",
+        str(pathlib.Path(__file__).parent.joinpath("conlleval.pl"))
+    ], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    gold, err = process.communicate(("\n".join(lines) + "\n").encode())
+    return gold
+
+
+def _test_evaluates(lines):
     """
     Perform a test case on a given result lines. Compares the result returned
     from this library with the results of the original perl script.
@@ -13,19 +22,14 @@ def _test_case(lines):
         lines: lines of sequence tagging prediction file.
     """
     res = report(evaluate(lines))
-    process = subprocess.Popen([
-        "perl",
-        str(pathlib.Path(__file__).parent.joinpath("conlleval.pl"))
-    ], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    gold, err = process.communicate(("\n".join(lines) + "\n").encode())
-    assert res.strip() == gold.decode().strip()
+    assert res.strip() == gold(lines).decode().strip()
 
 
 def test_all_cases():
     for fn in pathlib.Path(__file__).parent.joinpath("cases").glob("*.txt"):
         with open(fn, "r") as f:
             lines = [line.rstrip("\n") for line in f]
-        _test_case(lines)
+        _test_evaluates(lines)
 
 
 if __name__ == "__main__":
